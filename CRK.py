c347f118cda9a983857caf2656e96e683f5cedec
⎝◣⏝◢⎠SANTRI-CYBER-SANTUY⎝◣⏝◢⎠ <79558956+AKKUSANTUY@users.noreply.github.com>
2021-04-04 09:29:49 +0700

    Update CRK.py

--- CRK.py
+++ CRK.py
@@ -1,5 +1,73 @@
 #_*_coding:UTF-8_*_
 
+import os, sys
+
+print ("\033[30;1mSilahkan Masukkan Username & Password Scriptnya")
+
+print ("\033[33;1matau silahkan Hubungi Authornya +6282134566596 ")
+
+username = 'SANTRI'      
+
+password = 'CYBER'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[32;1mAkhirnya password bener juga cuk", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\033[31;1mPasswordnya salah cuk... [?]\033[33;1m"
+
+			print "Silahkan segera log-in kembali cuk...!!\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[30;1mUsernamenya salah cuk... [?]\033[31;1m"
+
+		print "Silahkan segera log-in kembali cuk...!!\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
+
 from os import system as sis
 from time import sleep as waktu
 import sys
